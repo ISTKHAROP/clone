49de1e18a983f05737fa6e4f451ab8fdd4571344
ISTKHAROP <istkharalam2006@gmail.com>
2026-05-31 21:23:54 +0530

    Update queue.py

--- PritiMusic/utils/inline/queue.py
+++ PritiMusic/utils/inline/queue.py
@@ -111,7 +111,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/betabot_hub",
+                url="https://t.me/KavyaBots",
             ),
         ],
     ]
