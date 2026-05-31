e1825c63d48fae9df156838e87c301740bf59adf
ISTKHAROP <istkharalam2006@gmail.com>
2026-06-01 00:01:37 +0530

    Update buttons.py

--- PritiMusic/cplugin/buttons.py
+++ PritiMusic/cplugin/buttons.py
@@ -47,9 +47,10 @@ def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
+    percentage = (played_sec / duration_sec) * 100
+umm = math.floor(percentage)
 
-    if 0 < umm <= 10:
+  if 0 < umm <= 10:
     bar = "˹✦━━━━━━━━━˼"
 elif 10 < umm < 20:
     bar = "˹━✦━━━━━━━━˼"
