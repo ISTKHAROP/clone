f41f6fac42d47cb3cb2edd68f32cc97b3ea0da59
ISTKHAROP <istkharalam2006@gmail.com>
2026-05-31 16:51:08 +0530

    Update clone.py

--- PritiMusic/cplugin/clone.py
+++ PritiMusic/cplugin/clone.py
@@ -19,7 +19,7 @@ def get_random_start_img():
         if isinstance(config.START_IMG_URL, list):
             return random.choice(config.START_IMG_URL)
         return config.START_IMG_URL
-    return "https://files.catbox.moe/10zwqs.jpg" # Fallback Image
+    return "https://files.catbox.moe/wktt8l.jpg" # Fallback Image
 
 
 @Client.on_message(filters.command("clone"))
@@ -31,7 +31,7 @@ async def ping_clone(client: Client, message: Message, _):
         caption=_["NO_CLONE_MSG"],
         reply_markup=InlineKeyboardMarkup(
             [
-                [styled_button("ɢᴏ ᴀɴᴅ ᴄʟᴏɴᴇ", url=BOT_LINK, style=ButtonStyle.SUCCESS)]
+                [styled_button(" ❖ 𝐆ᴏ 𝐀ηᴅ 𝐂ʟᴏηє ❖ ", url=BOT_LINK, style=ButtonStyle.SUCCESS)]
             ]
         )
     )
